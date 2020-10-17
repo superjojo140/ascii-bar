299ab9ba0f730c82f7c9ddf29f87b1940e30e54f
superjojo140 <plooploe@web.de>
2020-10-17 17:08:25 +0200

    Add comments (good job)

--- src/AsciiBar.ts
+++ src/AsciiBar.ts
@@ -1,20 +1,65 @@
 export class AsciiBar {
+    /**
+     * Format of the displayed statusbar
+     */
     public formatString = '#percent #bar';
+
+    /**
+    * Number of steps to finish progress
+    */
     public total = 100;
-    public elapsed = 0;
+
+    /**
+     * Startdate to calculate elapsed time (in milliseconds)
+     */
     public startDate = new Date().getTime();
-    public lastUpdate = new Date().getTime();
+
+    /**
+     * Which timespan to use for timing calculation - If you are unsure allways use false here!
+     */
     public lastUpdateForTiming = false;
-    public timeToFinish = 0;
-    public overallTime = 0;
-    public stream = process.stdout;
+
+    /**
+     * Width of the progress bar (only the #bar part)
+     */
     public width = 20;
+
+    /**
+    * Symbol for the done progress in the #bar part
+    */
     public doneSymbol = ">";
+
+    /**
+    * Symbol for the undone progress in the #bar part
+    */
     public undoneSymbol = "⋅";
+
+    /**
+    * Wether to print to configured stream or not
+    */
     public print = true;
+
+    /**
+     * A spinner object describing how the spinner looks like
+     * Change this for another spinner
+     */
     public spinner = defaultSpinner;
+
+    /**
+     * The message displayed at the #message placeholder
+     */
     public message = "";
 
+    /**
+    * wether to call progressbar's stop() function automatically if the progress reaches 100%
+    */
+    public autoStop = true;
+
+    private elapsed = 0;
+    private lastUpdate = new Date().getTime();
+    private timeToFinish = 0;
+    private overallTime = 0;
+    private stream = process.stdout;
     private spinnerTimeout;
     private enableSpinner = false;
     private currentSpinnerSymbol = "";
@@ -22,28 +67,36 @@ export class AsciiBar {
 
     constructor(options?: string | ProgressbarOptions) {
         if (options) {
+            //if only a string was provided, use this as formatString
             if (typeof options == "string") {
                 options = { formatString: options }
             }
 
+            //set other options
             for (const opt in options) {
-                this[opt] = options[opt];
+                if (this[opt] !== undefined) {
+                    this[opt] = options[opt];
+                }
             }
 
+            //set start value
+            if (options.start) {
+                this.current = options.start;
+            }
+
+            //enable spinner
             if (this.enableSpinner) {
                 this.spinnerTimeout = setTimeout(this.updateSpinner, this.spinner.interval);
             }
         }
     }
 
-    updateSpinner = () => {
-        this.spinner.currentFrame = (this.spinner.currentFrame + 1) % this.spinner.frames.length;
-        this.currentSpinnerSymbol = this.spinner.frames[this.spinner.currentFrame];
-        this.printLine();
-        this.spinnerTimeout = setTimeout(this.updateSpinner, this.spinner.interval);
-    }
 
-    renderLine() {
+    /**
+     * Creates the progressbar string with all configured settings
+     * @returns a string representating the progressbar
+     */
+    public renderLine(): string {
         let plusCount = Math.round(this.current / this.total * this.width);
         let minusCount = this.width - plusCount;
         let plusString = "";
@@ -74,14 +127,22 @@ export class AsciiBar {
         return line;
     }
 
-    printLine() {
+    /**
+     * Render the progressbar and print it to output stream
+     */
+    public printLine(): void {
         if (!this.print) { return; }
         this.stream.cursorTo(0);
         this.stream.write(this.renderLine());
         this.stream.clearLine(1);
     }
 
-    update(current: number, message?) {
+    /**
+     * update the progress. This will trigger re-rendering the progressbar
+     * @param current the new absolute progress value
+     * @param message [optional] update the message displayed at the #message placeholder
+     */
+    public update(current: number, message?: string) {
         this.current = current;
 
         if (message) { this.message = message; }
@@ -99,13 +160,28 @@ export class AsciiBar {
         this.printLine()
 
         //Stop if finished
-        if (this.current / this.total >= 1) { this.stop() }
+        if (this.autoStop && (this.current / this.total >= 1)) { this.stop() }
 
         this.lastUpdate = now;
         return this
     }
 
-    formatTime(millis: number) {
+    /**
+     * Updates the spinner if enabled
+     */
+    private updateSpinner = () => {
+        if (this.spinner.currentFrame === undefined) { this.spinner.currentFrame = 0}
+        this.spinner.currentFrame = (this.spinner.currentFrame + 1) % this.spinner.frames.length;
+        this.currentSpinnerSymbol = this.spinner.frames[this.spinner.currentFrame];
+        this.printLine();
+        this.spinnerTimeout = setTimeout(this.updateSpinner, this.spinner.interval);
+    }
+
+    /**
+     * Formats a time span (given in milliseconds) to a easy human readable string
+     * @param millis timespan in milliseconds
+     */
+    private formatTime(millis: number): string {
         //Milliseconds
         if (millis < 500) {
             return `${Math.round(millis)}ms`;
@@ -133,17 +209,28 @@ export class AsciiBar {
         return `${days}d ${hours}h ${minutes}m`;
     }
 
-    stop() {
+    /**
+     * Stop the progressbar
+     * This will stop the spinner and change it's symbol to a checkmark (if not disabled)
+     * Message will be changed to a string describing the elapsed time (if not disabled)
+     * This function will be triggered automatically if the progressbar reaches 100% (if not disabled)
+     * @param withInfo wether to auto-update the progressbar's spinner and message after stopping
+     */
+    public stop(withInfo = true) {
         //Stop the spinner
         if (this.spinnerTimeout) {
             clearTimeout(this.spinnerTimeout);
         }
-        //change spinner to checkmark
-        this.currentSpinnerSymbol = colorCodes.Green + colorCodes.Bright + "✓" + colorCodes.Reset;
-        //set overalltime to really elapsed time
-        this.overallTime = this.elapsed;
-        this.message = `Finished in ${this.formatTime(this.overallTime)}`
-        this.printLine();
+
+        if (withInfo) {
+            //change spinner to checkmark
+            this.currentSpinnerSymbol = colorCodes.Green + colorCodes.Bright + "✓" + colorCodes.Reset;
+            //set overalltime to really elapsed time
+            this.overallTime = this.elapsed;
+            this.message = `Finished in ${this.formatTime(this.overallTime)}`
+            this.printLine();
+        }
+
         //add newline
         console.log();
     }
@@ -151,16 +238,17 @@ export class AsciiBar {
 
 interface ProgressbarOptions {
     /**
-     * Format of the dipslayed statusbar
-     * @default '#percent #bar'
+     * Format of the displayed statusbar
      * Use serveral of this placeholders:
      * #bar #count #percent #overall #elapsed #ttf #message #spinner
-     * And combine with serveral of this formats:
+     * And combine with serveral of this formatters:
      * ##default ##green ##blue ##red ##yellow ##bright ##dim
+     * @default '#percent #bar'
      * @example '##bright##blue#spinner##default #percent #bar Elapsed: #elapsed Time to finish: #ttf  #message'
      */
 
     formatString?: string;
+
     /**
      * Number of steps to finish progress
      * @default 100
@@ -200,8 +288,8 @@ interface ProgressbarOptions {
 
     /**
      * Wether to print to configured stream or not
-     * @default true
      * If set to false get the currently rendered statusbar with bar.renderLine()
+     * @default true
      */
     print?: boolean;
 
@@ -209,23 +297,29 @@ interface ProgressbarOptions {
      * Start value of progress
      * @default 0
      */
-    current?: number;
+    start?: number;
 
     /**
      * Wether to enable the spinner update function or not.
-     * @default false
      * If enabled the statusbar will re-render automatically every few seconds to update the spinner symbol
      * Make sure to include #spinner in formatString to use spinner symbol
+     * @default false
      */
     enableSpinner?: boolean;
-    
+
     /**
-     * Wether to use last update for timing calculation or overall elapsed time
-     * If you are unsure allways use false here!
-     * @default: false (using overall elapsed time for timing calculation)
-     * If set to true: using elapsed time for the last step for timing calculation. WARNING: This implies, that every call of the ProgressBar.update() function increment the state with the same stepwidth.
+     * Which timespan to use for timing calculation - If you are unsure allways use false here!
+     * set to FALSE: Assume that each of the remaining steps will take as long as THE AVERAGE OF ALL the previous steps
+     * set to TRUE: Assume that eah of the remaining steps will take as long as THE LAST STEP took. WARNING: This implies, that every call of the ProgressBar.update() function increment the state with the same stepwidth.
+     * @default false using overall elapsed time for timing calculation
      */
     lastUpdateForTiming?: boolean;
+
+    /**
+     * wether to call progressbar's stop() function automatically if the progress reaches 100%
+     * @default true
+     */
+    autoStop?: boolean;
 }
 
 //ColorCodes from https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
@@ -250,12 +344,24 @@ const colorCodes = {
 
 let defaultSpinner: Spinner = {
     interval: 120,
-    frames: ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"],
-    currentFrame: 0
+    frames: ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"]
 }
 
 interface Spinner {
+    /**
+     * Number of milliseconds to update to the next spinner frame
+     */
     interval: number;
+
+    /**
+     * Array of the spinner "frames"
+     * A frame means one char
+     */
     frames: string[];
-    currentFrame: number;
+
+    /**
+     * Used in runtime to store currently displayed frame
+     * @default 0
+     */
+    currentFrame?: number;
 }
